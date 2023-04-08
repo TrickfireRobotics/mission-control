b90bc25a25704970bc626eb42d05eeb216a0206c
adamseth2 <chhoradam@gmail.com>
2023-04-07 23:39:11 -0700

    inital commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+// <script setup lang="ts">
+import BasicInfo from './components/BasicInfo.vue'
+export default {
+  components: {
+    BasicInfo
+  }
+}
+</script>
+<template>
+  <BasicInfo />
+</template>
+
+<style></style>
---
+++ src/components/BasicInfo.vue
@@ -0,0 +1,24 @@
+<script>
+import ROSLIB from '../.././package/roslib.js'
+console.log('hello')
+// Create ros object to communicate over your Rosbridge connection
+const ros = new ROSLIB.Ros({ url: 'ws://localhost:9090' })
+
+// When the Rosbridge server connects, fill the span with id “status" with “successful"
+ros.on('connection', () => {
+  document.getElementById('status').innerHTML = 'successful'
+})
+
+// When the Rosbridge server experiences an error, fill the “status" span with the returned error
+ros.on('error', (error) => {
+  document.getElementById('status').innerHTML = `errored out (${error})`
+})
+
+// When the Rosbridge server shuts down, fill the “status" span with “closed"
+ros.on('close', () => {
+  document.getElementById('status').innerHTML = 'closed'
+})
+</script>
+<template>
+  <div id="status">Didn't work</div>
+</template>
