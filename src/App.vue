4dd47f69a9812d5af2cb73617041c3105fd48a27
adamseth2 <chhoradam@gmail.com>
2023-05-30 10:44:35 -0700

    remade App.vue into composition API

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
-<script>
-// <script setup lang="ts">
+<script setup lang="ts">
 import Navbar from './components/Navbar.vue'
 import MotorGrid from './components/MotorGrid.vue'
-export default {
-  components: {
-    Navbar,
-    MotorGrid
-  }
-}
+import CameraContainer from './components/CameraContainer.vue'
 </script>
 <template>
-  <Navbar />
-  <MotorGrid />
+  <div id="page">
+    <Navbar />
+    <CameraContainer />
+    <MotorGrid />
+  </div>
 </template>
 
 <style lang="scss">
@@ -20,5 +17,17 @@ export default {
 ::after {
   margin: 0;
   padding: 0;
+  list-style-type: none;
+}
+#page {
+  display: grid;
+  width: 100%;
+  // height: 100vh;
+  grid-template-areas:
+    'nav nav nav'
+    'camera camera motor-grid'
+    'Something Something Something';
+  grid-template-rows: 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr 1fr;
 }
 </style>
