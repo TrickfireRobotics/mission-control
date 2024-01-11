4536c3dde113c2e8f79941f82af0d87eacc74a43
VladimirKupryukhin <vladimirkupryukhin@gmail.com>
2024-01-10 20:03:12 -0800

    Proof of concept for a controller

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,59 +1,61 @@
 <script lang="ts" setup>
-import ROSLIB, { Ros } from 'roslib';
-import { inject, onMounted, ref } from 'vue';
-//guarantee ros is defined
-// use this instead want to handle case where ros is undefined
-// const ros = inject<Ros>('ros')
-const webSocketStatus = inject<boolean>('isWebSocketConnected', false);
+import { onMounted, ref } from 'vue'
+import {startControllerCode} from "../script/controller.js"; 
 
-//TODO FIX CODE
-const latency = ref(-1);
-// const latency_listener = new ROSLIB.Topic({
-//   ros,
-//   name: '/latency',
-//   messageType: 'std_msgs/String'
-// })
-// onMounted(() => {
-//   console.log('Created')
-//   // Create ros object to communicate over your Rosbridge connection
-//   const ros = new ROSLIB.Ros({ url: 'ws://localhost:9090' })
-//   // When the Rosbridge server connects, fill the span with id “status" with “successful"
-//   ros.on('connection', () => {
-//     console.log(this)
-//     webSocketStatus.value = true
-//     // document.getElementById('status').innerHTML = 'successful'
-//     // @ts-ignore
-//     ros.on('error', (error) => {
-//       webSocketStatus.value = false
-//       console.log(error)
-//       // document.getElementById('status').innerHTML = `errored out (${error})`
-//     })
+const webSocketStatus = ref(false)
+const latency = ref(-1)
 
-//     // When the Rosbridge server shuts down, fill the “status" span with “closed"
-//     ros.on('close', () => {
-//       webSocketStatus.value = false
-//       // document.getElementById('status').innerHTML = 'closed'
-//     })
-//   })
-//   //listens to mission_control
-//   // @ts-ignore
-//   const latency_listener = new ROSLIB.Topic({
-//     ros,
-//     name: '/latency',
-//     messageType: 'std_msgs/String'
-//   })
-//   // When we receive a message on /my_topic, add its data as a list item to the “messages" ul
-//   // @ts-ignore
-//   latency_listener.subscribe((message) => {
-//     let currTime = Date.now()
-//     let start = message.data.indexOf('=')
-//     let end = message.data.indexOf(',')
-//     let rosTime = message.data.substring(start + 1, end)
-//     let decimalPlaces = 1
-//     rosTime = rosTime.substring(0, currTime.toString().length)
-//     latency.value = currTime - rosTime
-//   })
-// })
+onMounted(() => {
+  console.log('Created')
+  // Create ros object to communicate over your Rosbridge connection
+  // @ts-ignore
+  const ros = new ROSLIB.Ros({ url: 'ws://localhost:9090' })
+
+  // When the Rosbridge server connects, fill the span with id “status" with “successful"
+  ros.on('connection', () => {
+    console.log(this)
+    webSocketStatus.value = true
+    startControllerCode(ros)
+    // document.getElementById('status').innerHTML = 'successful'
+    // @ts-ignore
+    ros.on('error', (error) => {
+      webSocketStatus.value = false
+      console.log(error)
+      // document.getElementById('status').innerHTML = `errored out (${error})`
+    })
+
+    // When the Rosbridge server shuts down, fill the “status" span with “closed"
+    ros.on('close', () => {
+      webSocketStatus.value = false
+      // document.getElementById('status').innerHTML = 'closed'
+    })
+  })
+  //listens to mission_control
+  // @ts-ignore
+  const latency_listener = new ROSLIB.Topic({
+    ros,
+    name: '/latency',
+    messageType: 'std_msgs/String'
+  })
+  
+  // When we receive a message on /my_topic, add its data as a list item to the “messages" ul
+  // @ts-ignore
+  latency_listener.subscribe((message) => {
+    let currTime = Date.now()
+    let start = message.data.indexOf('=')
+    let end = message.data.indexOf(',')
+    let rosTime = message.data.substring(start + 1, end)
+    let decimalPlaces = 1
+    rosTime = rosTime.substring(0, currTime.toString().length)
+    latency.value = currTime - rosTime
+  })
+  
+
+
+  
+})
+
+// When the Rosbridge server experiences an error, fill the “status" span with the returned error
 </script>
 <template>
   <nav>
