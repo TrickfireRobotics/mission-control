9050d908d086a8b44d90e1c98a237075eab00b38
VladFigy <vladimirkupryukhin@gmail.com>
2024-08-31 17:08:59 -0700

    Update button states according to current rover arm state

--- src/components/ArmModeSelection.vue
+++ src/components/ArmModeSelection.vue
@@ -8,23 +8,38 @@ onMounted(() => initialize());
 
 let service : ROSLIB.Service;
 
+/**
+ * disabled = 0
+ * individual_motor_control_vel = 1
+ * individual_motor_control_pos = 2
+ * inverse_kinematics = 3
+ */
+let current_arm_mode = ref(-1) // Default is disabled
+//let isLoaded = ref(false);
+
+
 function initialize() {
     if (myRos != null) {
         service = new ROSLIB.Service({
             ros: myRos,
             name: "/get_arm_mode",
-            serviceType: "ResponseStructure"
+            serviceType: "ArmMode"
         })
+
+        getCurrentArmMode()
+
     }
 }
 
-function sendRequest() {
-    var request = {
+function getCurrentArmMode() {
+    var request = { //Dummy data for now
         input: 1
     }
 
     service.callService(request, function(result) {
-        console.log("Result from service")
+        console.log("Result from service " + result.current_mode)
+        current_arm_mode.value = result.current_mode
+        //isLoaded.value = true
     })
 
 
@@ -40,11 +55,11 @@ function sendRequest() {
                 <b class="mode-text">Arm Control Mode:</b>
             </div>
             <div>
-                <button class="button-mode" @click="sendRequest">TEST</button>
-                <button class="button-mode">Individual Motor Control (Velocity)</button>
-                <button class="button-mode">Individual Motor Control (Position)</button>
-                <button class="button-mode">Inverse Kinematics</button>
-                <button class="button-mode">Disabled</button>
+                <!--button class="button-mode" @click="sendRequest">TEST</button-->
+                <button v-bind:class="{'button-mode-unselected': current_arm_mode != 0, 'button-mode-selected': current_arm_mode == 0,}">Disabled</button>
+                <button v-bind:class="{'button-mode-unselected': current_arm_mode != 1, 'button-mode-selected': current_arm_mode == 1,}">Individual Motor Control (Velocity)</button>
+                <button v-bind:class="{'button-mode-unselected': current_arm_mode != 2, 'button-mode-selected': current_arm_mode == 2,}">Individual Motor Control (Position)</button>
+                <button v-bind:class="{'button-mode-unselected': current_arm_mode != 3, 'button-mode-selected': current_arm_mode == 3,}">Inverse Kinematics</button>
             </div>
         </div>
     </div>
@@ -68,9 +83,19 @@ function sendRequest() {
     flex-direction: column;
 }
 
-.button-mode {
+.button-mode-unselected {
     margin: 5px;
-    background-color: rgb(48, 182, 48);
+    background-color: rgb(201, 56, 56);
+    color: white;
+    padding: 8px;
+    font-size: 16px;
+    border: none;
+    border-radius: 10px;
+}
+
+.button-mode-selected{
+    margin: 5px;
+    background-color: rgb(56, 201, 68);
     color: white;
     padding: 8px;
     font-size: 16px;
