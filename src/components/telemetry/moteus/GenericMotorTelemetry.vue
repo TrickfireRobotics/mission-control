fa5b07435232d208d493e3c2d71c41dc4bf738df
VladFigy <vladimirkupryukhin@gmail.com>
2024-09-29 19:27:42 -0700

    Telemetry.vue uses v-for now. Added RobotInfo

--- src/components/telemetry/moteus/GenericMotorTelemetry.vue
+++ src/components/telemetry/moteus/GenericMotorTelemetry.vue
@@ -10,28 +10,14 @@ import ROSLIB, { Ros } from 'roslib';
 
 onMounted(() => initialize());
 
-const props = defineProps(['moteusCANID', 'displayName', 'preset', 'dataSub', 'update_ms', 'showAllFeatures']);
+const props = defineProps(['displayName', 'dataSourceMethod', 'update_ms', 'showAllFeatures', 'motorType']);
 
-const possiblePresets = ["FULL", "IMPORTANT"];
-
-enum ChoiceToIndex {
-  CAN_ID = 0,
-  POSITION = 1,
-  VELOCITY = 2,
-  TORQUE = 3,
-  TEMPERATURE = 4,
-  POWER = 5,
-  INPUT_VOLTAGE = 6,
-  Q_CURRENT = 7,
-  D_CURRENT = 8
-}
 
 let isRecordingData = false;
 let csvData;
 
 // We use this to fill up csv data
 //Each element is another array that holds the actual data
-let csvDataObjects: object[] = [];
 let showCheckbox = ref(true);
 
 let recordButtonText = ref("Start Recording")
@@ -48,158 +34,17 @@ const myRos = inject<Ros>('ros')
  * It should be possible to simply add another entry to this, and everyting should
  * show up, assuming that the data recieved from the the rover also has these values
  */
-// const moteuesDataChoice = [
-//   {
-//     prettyName: "CAN ID",
-//     identifier: "canID",
-//     dataValue: ref("N/A"),
-//     isSelected: ref(false),
-//     shouldRecordData: ref(true)
-//   },
-//   {
-//     prettyName: "Position",
-//     identifier: "position",
-//     dataValue: ref("N/A"),
-//     isSelected: ref(false),
-//     shouldRecordData: ref(true)
-//   },
-//   {
-//     prettyName: "Velocity",
-//     identifier: "velocity",
-//     dataValue: ref("N/A"),
-//     isSelected: ref(false),
-//     shouldRecordData: ref(true)
-//   },
-//   {
-//     prettyName: "Torque",
-//     identifier: "torque",
-//     dataValue: ref("N/A"),
-//     isSelected: ref(false),
-//     shouldRecordData: ref(true)
-//   },
-//   {
-//     prettyName: "Temperature",
-//     identifier: "temperature",
-//     dataValue: ref("N/A"),
-//     isSelected: ref(false),
-//     shouldRecordData: ref(true)
-//   },
-//   {
-//     prettyName: "Power",
-//     identifier: "power",
-//     dataValue: ref("N/A"),
-//     isSelected: ref(false),
-//     shouldRecordData: ref(true)
-//   },
-//   {
-//     prettyName: "Input Voltage",
-//     identifier: "inputVoltage",
-//     dataValue: ref("N/A"),
-//     isSelected: ref(false),
-//     shouldRecordData: ref(true)
-//   },
-//   {
-//     prettyName: "Q Current",
-//     identifier: "q_current",
-//     dataValue: ref("N/A"),
-//     isSelected: ref(false),
-//     shouldRecordData: ref(true)
-//   },
-//   {
-//     prettyName: "D Current",
-//     identifier: "d_current",
-//     dataValue: ref("N/A"),
-//     isSelected: ref(false),
-//     shouldRecordData: ref(true)
-//   }
 
-// ]
 
-const moteuesDataChoice = ref([
-  {
-    prettyName: "CAN ID",
-    identifier: "canID",
-    dataValue: ref("N/A"),
-    isSelected: ref(true),
-    shouldRecordData: ref(true)
-  }
-  
-
-])
+const moteuesDataChoice = ref([])
 
 
 function initialize() {
-  //console.log(moteuesDataChoice.value[0].dataValue)
-  moteuesDataChoice.value[0].dataValue = props.moteusCANID;
   csvData = new SaveCSVData()
 
-  if (myRos != null) {
-    getMoteusMotorStateService = new ROSLIB.Service({
-      ros: myRos,
-      name: "/get_moteus_motor_state",
-      serviceType: "MoteusState"
-    })
-  }
-
-  // // FULL
-  // if (props.preset == possiblePresets[0]) {
-  //   moteuesDataChoice.value[ChoiceToIndex.CAN_ID].isSelected = true;
-  //   moteuesDataChoice.value[ChoiceToIndex.POSITION].isSelected = true;
-  //   moteuesDataChoice.value[ChoiceToIndex.VELOCITY].isSelected = true;
-  //   moteuesDataChoice.value[ChoiceToIndex.TORQUE].isSelected = true;
-  //   moteuesDataChoice.value[ChoiceToIndex.TEMPERATURE].isSelected = true;
-  //   moteuesDataChoice.value[ChoiceToIndex.POWER].isSelected = true;
-  //   moteuesDataChoice.value[ChoiceToIndex.INPUT_VOLTAGE].isSelected = true;
-  //   moteuesDataChoice.value[ChoiceToIndex.Q_CURRENT].isSelected = true;
-  //   moteuesDataChoice.value[ChoiceToIndex.D_CURRENT].isSelected = true;
-
-  // }
-  // // IMPORTANT
-  // else if (props.preset == possiblePresets[1]) {
-  //   moteuesDataChoice[ChoiceToIndex.CAN_ID].isSelected.value = false;
-  //   moteuesDataChoice[ChoiceToIndex.POSITION].isSelected.value = true;
-  //   moteuesDataChoice[ChoiceToIndex.VELOCITY].isSelected.value = true;
-  //   moteuesDataChoice[ChoiceToIndex.TORQUE].isSelected.value = false;
-  //   moteuesDataChoice[ChoiceToIndex.TEMPERATURE].isSelected.value = false;
-  //   moteuesDataChoice[ChoiceToIndex.POWER].isSelected.value = false;
-  //   moteuesDataChoice[ChoiceToIndex.INPUT_VOLTAGE].isSelected.value = false;
-  //   moteuesDataChoice[ChoiceToIndex.Q_CURRENT].isSelected.value = false;
-  //   moteuesDataChoice[ChoiceToIndex.D_CURRENT].isSelected.value = false;
-  // }
-
-  // We enter this interval for at least once
-  //pollingData = setInterval(readDataCallback, 500);//default 100
-  //setInterval(requestMoteusState, 500)
-
-  setInterval(test, 2000);
 
 };
-let counter = 0;
-function test(){
-  let entry = {
-    prettyName: "HELP" + counter,
-    identifier: "help",
-    dataValue: "N/A",
-    isSelected: true,
-    shouldRecordData: true
-  }
 
-  moteuesDataChoice.value.push(entry);
-  console.log("added")
-  counter++
-}
-
-
-function requestMoteusState() {
-  var request = {
-    target_can_id: parseInt(moteuesDataChoice.value[0].dataValue)
-  }
-
-  getMoteusMotorStateService.callService(request, function (result) {
-    //console.log("Result from service " + result.json_payload)
-    updateUIWithNewData(result.json_payload)
-  })
-}
 
 function updateUIWithNewData(jsonString) {
   let json = JSON.parse(jsonString)
@@ -217,14 +62,6 @@ function updateUIWithNewData(jsonString) {
 
   //check if this entry is ours via canid
   if (true) {
-    updateEntry(moteuesDataChoice[ChoiceToIndex.POSITION], json.position)
-    updateEntry(moteuesDataChoice[ChoiceToIndex.VELOCITY], json.velocity)
-    updateEntry(moteuesDataChoice[ChoiceToIndex.TORQUE], json.torque)
-    updateEntry(moteuesDataChoice[ChoiceToIndex.POWER], json.power)
-    updateEntry(moteuesDataChoice[ChoiceToIndex.INPUT_VOLTAGE], json.input_voltage)
-    updateEntry(moteuesDataChoice[ChoiceToIndex.Q_CURRENT], json.q_current)
-    updateEntry(moteuesDataChoice[ChoiceToIndex.D_CURRENT], json.d_current)
-
     // Data recording; only do it if recording
     if (isRecordingData) {
       constructRecordingEntry()
@@ -330,10 +167,10 @@ function readDataCallback() {
  * 
  */
 function itemClicked(itemName: String) {
-  // let mything = getMoteusDataObjectFromIdentifier(itemName);
-  // if (mything != null) {
-  //   mything.isSelected.value = !mything.isSelected.value;
-  // }
+  let mything = getMoteusDataObjectFromIdentifier(itemName);
+  if (mything != null) {
+    mything.isSelected.value = !mything.isSelected.value;
+  }
 
 
 }
@@ -344,52 +181,52 @@ function itemClicked(itemName: String) {
  * After stopping the recording, it will build the csv file
  */
 function recordButtonPressed() {
-  // if (!isRecordingData) {
-  //   recordButtonText.value = "Stop Recording"
-  //   showCheckbox.value = false;
+  if (!isRecordingData) {
+    recordButtonText.value = "Stop Recording"
+    showCheckbox.value = false;
 
-  //   csvData = new SaveCSVData()
+    csvData = new SaveCSVData()
 
-  //   let header: string[] = [];
+    let header: string[] = [];
 
-  //   for (let index = 0; index < moteuesDataChoice.length; index++) {
-  //     let entry = moteuesDataChoice[index];
+    for (let index = 0; index < moteuesDataChoice.value.length; index++) {
+      let entry = moteuesDataChoice[index];
 
-  //     if (entry.shouldRecordData.value == true) {
-  //       header.push(entry.identifier)
-  //     }
-  //   }
+      if (entry.shouldRecordData.value == true) {
+        header.push(entry.identifier)
+      }
+    }
 
-  //   csvData.setHeader(header)
-  // }
-  // else {
-  //   recordButtonText.value = "Start Recording"
-  //   showCheckbox.value = true
+    csvData.setHeader(header)
+  }
+  else {
+    recordButtonText.value = "Start Recording"
+    showCheckbox.value = true
 
-  //   csvData.saveToFile(props.displayName);
-  // }
+    csvData.saveToFile(props.displayName);
+  }
 
-  // isRecordingData = !isRecordingData;
+  isRecordingData = !isRecordingData;
 }
 
 function getMoteusDataObjectFromIdentifier(itemName: String) {
-  // for (let index = 0; index < moteuesDataChoice.length; index++) {
-  //   if (moteuesDataChoice[index].identifier == itemName) {
-  //     return moteuesDataChoice[index];
-  //   }
-  // }
+  for (let index = 0; index < moteuesDataChoice.value.length; index++) {
+    if (moteuesDataChoice[index].identifier == itemName) {
+      return moteuesDataChoice[index];
+    }
+  }
 
-  // return null;
+  return null;
 }
 
 function getMoteusDataObjectFromName(itemName: String) {
-  // for (let index = 0; index < moteuesDataChoice.length; index++) {
-  //   if (moteuesDataChoice[index].prettyName == itemName) {
-  //     return moteuesDataChoice[index];
-  //   }
-  // }
+  for (let index = 0; index < moteuesDataChoice.length; index++) {
+    if (moteuesDataChoice[index].prettyName == itemName) {
+      return moteuesDataChoice[index];
+    }
+  }
 
-  // return null;
+  return null;
 }
 
 /**
@@ -397,11 +234,11 @@ function getMoteusDataObjectFromName(itemName: String) {
  * All this does it hold the logical state
  */
 function checkboxClicked(name: String) {
-  // let dataEntry = getMoteusDataObjectFromName(name)
-  // if (dataEntry != null) {
-  //   dataEntry.shouldRecordData.value = !dataEntry.shouldRecordData.value
-  //   console.log(name + "" + dataEntry.shouldRecordData.value)
-  // }
+  let dataEntry = getMoteusDataObjectFromName(name)
+  if (dataEntry != null) {
+    dataEntry.shouldRecordData.value = !dataEntry.shouldRecordData.value
+    console.log(name + "" + dataEntry.shouldRecordData.value)
+  }
 }
 
 defineExpose({ recordButtonPressed })
@@ -425,22 +262,22 @@ defineExpose({ recordButtonPressed })
             v-bind:itemName="item.prettyName" v-bind:isSelected="item.isSelected"></DropDownItem>
 
         </div>
-
-
       </div>
 
-      <!--div class="moteus-reminder">Motor Controller: <b>Moteus</b></div-->
-
-    </div>
-
-    <div>
+      <div>
       <button v-if="showAllFeatures" id="record_button"
         v-bind:class="{ 'record-button-green': !isRecordingData, 'record-button-red': isRecordingData }"
         @click="recordButtonPressed">{{ recordButtonText }}</button>
+      </div>
+
+      
+
     </div>
 
 
     <div class="flex-container flex-vertical">
+      <div class="moteus-reminder">Type: <b>{{ motorType }}</b></div>
+      
       <TelemetryDataDisplay v-for="(item) in moteuesDataChoice" v-bind:itemName="item.prettyName"
         v-bind:isSelected="item.isSelected" v-bind:value="item.dataValue"
         v-bind:shouldRecordData="item.shouldRecordData" @checkboxClicked="checkboxClicked"
@@ -504,7 +341,7 @@ defineExpose({ recordButtonPressed })
   font-size: 16px;
   border: none;
   border-radius: 10px;
-  margin-top: 5px;
+  margin-left: 5px;
 }
 
 .record-button-red {
@@ -514,7 +351,8 @@ defineExpose({ recordButtonPressed })
   font-size: 16px;
   border: none;
   border-radius: 10px;
-  margin-top: 5px;
+  margin-left: 5px;
+  
 }
 
 
