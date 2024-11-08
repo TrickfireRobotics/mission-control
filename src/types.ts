addd645fbe67b0b7d573609e4720323142be4a2e
Adam Chhor <chhoradam@gmail.com>
2024-11-08 05:31:43 -0800

    added conditonal Types, so primative types are inferred from Ros types

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,1 @@
-export type CompressedImage = {
-  Header: {
-    //header is not confirmed on types
-    frame_id: string;
-    stamp: { sec: number; nanosec: number };
-  };
-  format: 'jpg' | 'png';
-  data: string; //hexadecimal
-};
+// Any Global Types go here
