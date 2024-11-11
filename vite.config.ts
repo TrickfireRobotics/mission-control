b97336e21d9f3dc822eee967a648708409873a82
Adam Chhor <chhoradam@gmail.com>
2024-11-11 00:59:25 -0800

    removed dev tools, because caused a huge compile time, later will add them again but as a option

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,9 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-import vueDevTools from 'vite-plugin-vue-devtools';
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [vue()],
   server: {
     // automatically opens in browser
     open: true,
