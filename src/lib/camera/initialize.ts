92e37f9de4bcd62026746dad1616f14f94aaf8f8
Adam Chhor <chhoradam@gmail.com>
2024-11-10 18:28:55 -0800

    showing example of functions vs classes

--- src/lib/camera/initialize.ts
+++ src/lib/camera/initialize.ts
@@ -1,26 +1,31 @@
-import type { TopicType } from "@/store/rosTypes";
-import { useRoslibStore } from "@/store/useRoslib";
+import type { TopicType } from '@/store/rosTypes';
+import { useRoslibStore } from '@/store/useRoslib';
 
-function setUpCamera(startI : number, endI : number) {
+function setUpCamera(startI: number, endI: number) {
   // name: `/video_frames${i}`,
-  const cameraTopicType : TopicType  = "sensor_msgs/msg/CompressedImage";
-  const roslib = useRoslibStore()
-  for (let i = startI; i <= endI; i++) {
-    
-    const cameraTopicName = `/video_frames{i}`;
-    roslib.createPublisher({topicName : cameraTopicName, topicType : cameraTopicType })
-    // console.log(cameraTopic);
-    cameraTopic.subscribe<CompressedImage>((message) => {
-      const base64 = btoa(
-        Array(message.data.length)
-          .fill('')
-          .map((_, i) => String.fromCharCode(message.data[i]))
-          .join(''),
-      );
-      compressedImage[i] = 'data:image/jpg;base64,' + base64;
-    });
-  }
-  return compressedImage;
-}
-}
+  const cameraTopicType: TopicType = 'sensor_msgs/msg/CompressedImage';
+  const roslib = useRoslibStore();
 
+  // Error: Cannot redeclare block-scoped variable 'cameraSubs'
+  const cameraSubs = [];
+  for (let i = startI; i <= endI; i++) {
+    const cameraTopicName = `/video_frames{i}`;
+    const [data, subscribe, unsubscribe, isOn] = roslib.createSubscriber({
+      topicName: cameraTopicName,
+      topicType: cameraTopicType,
+    });
+    // Error: Argument of type '(Ref<boolean, boolean> | Ref<string | undefined, string | undefined> | ((options: { callback?: ((message: StdMsg<string>) => void) | undefined; defaultValue?: string | undefined; isDebugging?: boolean | undefined; }) => void))[]' is not assignable to parameter of type 'never'.
+    cameraSubs.push([data, subscribe, unsubscribe, isOn]);
+  }
+
+  // WHAT IT WOULD LOOK LIKE instead if uses classses
+  const cameraSubs: Subscriber[] = [];
+  for (let i = startI; i <= endI; i++) {
+    const cameraTopicName = `/video_frames{i}`;
+    const cameraSub: Subscriber = roslib.createSubscriber({
+      topicName: cameraTopicName,
+      topicType: cameraTopicType,
+    });
+    cameraSubs.push(cameraSub);
+  }
+}
