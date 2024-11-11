f64eae8c4529308deaabe5e3b8cb3345350bd295
uellenberg <JonahUellenberg@hotmail.com>
2024-11-10 18:17:29 -0800

    Fix build errors

--- src/lib/interface/robotInfo.ts
+++ src/lib/interface/robotInfo.ts
@@ -1,19 +1,17 @@
 import ROSLIB from 'roslib';
 
 export default class RobotInfo {
-  myROS = null;
-  getMoteusMotorStateService = null;
+  myROS: ROSLIB.Ros;
+  getMoteusMotorStateService: ROSLIB.Service;
 
-  constructor(givenROS) {
+  constructor(givenROS: ROSLIB.Ros) {
     this.myROS = givenROS;
 
-    if (this.myROS !== null) {
-      this.getMoteusMotorStateService = new ROSLIB.Service({
-        ros: this.myROS,
-        name: '/get_moteus_motor_state',
-        serviceType: 'MoteusState',
-      });
-    }
+    this.getMoteusMotorStateService = new ROSLIB.Service({
+      ros: this.myROS,
+      name: '/get_moteus_motor_state',
+      serviceType: 'MoteusState',
+    });
   }
 
   getMoteusMotorState(canfdID: number, callback) {
