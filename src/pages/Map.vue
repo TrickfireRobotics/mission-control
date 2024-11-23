73286b3f08c118871827912be82019dd59661e93
Adam Chhor <35408139+adamseth2@users.noreply.github.com>
2024-11-22 16:54:29 -0800

    made map 2x2 (#41)

--- src/pages/Map.vue
+++ src/pages/Map.vue
@@ -1,18 +1,18 @@
 <!-- Display map where the rover is, the target and line the rover will take -->
 <script setup lang="ts"></script>
 <template>
-  <!-- <h1>Not yet Implemented</h1> -->
-  <!-- <h1>Not yet Implemented</h1> -->
-  <!-- <h1>Not yet Implemented</h1> -->
-  <img id="map" src="../assets/image.png" alt="" />
-  <!-- <h1>Not yet Implemented</h1> -->
-  <!-- <h1>Not yet Implemented</h1> -->
-  <!-- <h1>Not yet Implemented</h1> -->
+  <div class="two-by-three-grid-page">
+    <img id="map" src="../assets/image.png" alt="" />
+    <h1>Not yet Implemented</h1>
+    <h1>Not yet Implemented</h1>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 #map {
   height: 100%;
   width: 100%;
+  grid-column: 1 / span 2;
+  grid-row: 1 / span 2;
 }
 </style>
