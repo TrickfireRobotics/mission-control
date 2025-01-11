f8cff215bcfd23d1fb8f4672569d312bac805cb2
Alan Talavera-Cordova <126989249+YoloMcFroyo@users.noreply.github.com>
2025-01-10 16:54:53 -0800

    [devtooltoggle] Add toggle for devtools

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,9 @@ onMounted(() => {
   // TODO: use environment variables to launch the different ports
   // Create ros object to communicate over your Rosbridge connection
   // local host development
-  // rosjs.init('ws://localhost:9090');
+  rosjs.init('ws://localhost:9090');
   // Connects to the router
-  rosjs.init('ws://192.168.0.145:9090');
+  // rosjs.init('ws://192.168.0.145:9090');
 
   // Connects to rover
   // rosjs.init('ws://10.0.0.10:9090');
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,13 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import { templateCompilerOptions } from '@tresjs/core';
 import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools'
 
+const useDevTools = process.env.USE_VITE_DEVTOOLS === 'true';
+const plugin = useDevTools ? [vueDevTools()] : [];
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue({ ...templateCompilerOptions })],
+  plugins: [vue({ ...templateCompilerOptions }), ...plugin],
   server: {
     // automatically opens in browser
     open: true,
