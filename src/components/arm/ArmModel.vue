69f25017bf8e89335a3a549a1deaf0d5d6ddee40
Adam Chhor <chhoradam@gmail.com>
2025-01-30 16:55:30 -0800

    [armmodelcleanup] made arm follow module pattern

---
+++ src/components/arm/ArmModel.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { shallowRef, watch } from 'vue';
+import { TresCanvas } from '@tresjs/core';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { useLoader } from '@tresjs/core';
+import { OrbitControls, GLTFModel } from '@tresjs/cientos';
+import type { Object3D } from 'three';
+
+const modelRef = shallowRef<Object3D>();
+// const { scene } = await useLoader(GLTFLoader, 'arm.glb');
+
+watch(modelRef, (model) => {
+  if (!model) return;
+  console.log('Model is rendered');
+});
+</script>
+<template>
+  <div id="arm">
+    <TresCanvas shadows alpha>
+      <TresAmbientLight :intensity="1" />
+      <TresDirectionalLight :intensity="2" :position="[3, 3, 3]" />
+      <TresPerspectiveCamera :position="[500, 500, 500]" />
+      <OrbitControls :enable-damping="false" :enable-pan="false" />
+      <Suspense>
+        <GLTFModel ref="modelRef" path="arm.glb" draco :rotation="[0, 0, 0]" />
+      </Suspense>
+    </TresCanvas>
+  </div>
+</template>
+<style lang="scss" scoped>
+#arm {
+  height: 100%;
+  width: 100%;
+  grid-column: 2 / span 2;
+  grid-row: 1 / span 2;
+}
+</style>
--- src/pages/Arm.vue
+++ src/pages/Arm.vue
@@ -1,61 +1,21 @@
 <!-- 3D model of the arm, camera arms, Any information related to the arm should be here  -->
 <script setup lang="ts">
-import { shallowRef, watch } from 'vue';
-import { TresCanvas } from '@tresjs/core';
-import { OrbitControls, GLTFModel } from '@tresjs/cientos';
+import { shallowRef } from 'vue';
 import ArmModeSelection from '../components/arm/ArmModeSelection.vue';
-import type THREE from 'three';
+import ArmModel from '../components/arm/ArmModel.vue';
+import type { Object3D } from 'three';
 
-const modelRef = shallowRef<THREE.Object3D>();
-
-watch(modelRef, (model) => {
-  if (!model) return;
-
-  // Move model so base is in the center
-  model.position.set(0, 100, -100);
-
-  // Make sure model faces up
-  model.rotation.set(90, 0, 0);
-});
-</script>
-<template>
-  <div class="page">
-    <ArmModeSelection></ArmModeSelection>
-    <div class="model-container">
-      <TresCanvas shadows alpha>
-        <TresAmbientLight :intensity="1" />
-        <TresDirectionalLight :intensity="2" :position="[3, 3, 3]" />
-        <TresPerspectiveCamera />
-        <OrbitControls :enable-damping="false" :enable-pan="false" />
-        <Suspense>
-          <GLTFModel ref="modelRef" path="arm.glb" draco />
-        </Suspense>
-      </TresCanvas>
-    </div>
-  </div>
-</template>
-
-<style lang="scss" scoped>
-.page {
-}
-
-.model-container {
-  height: 600px;
-}
-</style>
-<!-- 3D model of the arm, camera arms, Any information related to the arm should be here
-<script setup lang="ts">
-import ArmModeSelection from '../components/arm/ArmModeSelection.vue';
+const modelRef = shallowRef<Object3D>();
 </script>
 <template>
   <div class="two-by-three-grid-page">
-    <h1>Not yet Implemented</h1>
-    <h1>Not yet Implemented</h1>
-    <ArmModeSelection></ArmModeSelection>
-    <h1>Not yet Implemented</h1>
-    <h1>Not yet Implemented</h1>
+    <ArmModeSelection />
+    <Suspense>
+      <ArmModel />
+    </Suspense>
+
     <h1>Not yet Implemented</h1>
   </div>
 </template>
 
-<style lang="scss" scoped></style> -->
+<style lang="scss" scoped></style>
