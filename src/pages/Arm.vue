7741c9d51af064cbafd6cd411825a0b588bc02ae
Adam Chhor <chhoradam@gmail.com>
2025-02-07 17:07:54 -0800

    [armmodelcleanup] fixed eslint issues

--- src/pages/Arm.vue
+++ src/pages/Arm.vue
@@ -1,11 +1,7 @@
 <!-- 3D model of the arm, camera arms, Any information related to the arm should be here  -->
 <script setup lang="ts">
-import { shallowRef } from 'vue';
 import ArmModeSelection from '../components/arm/ArmModeSelection.vue';
 import ArmModel from '../components/arm/ArmModel.vue';
-import type { Object3D } from 'three';
-
-const modelRef = shallowRef<Object3D>();
 </script>
 <template>
   <div class="two-by-three-grid-page">
