1627df685bfcdc61036ad929f21b1e0246579401
AbbyHoyt <140842210+AbbyHoyt@users.noreply.github.com>
2025-02-26 17:37:37 -0800

    Birdseye view of rover for SAR

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,10 +1,12 @@
 <!--
 Show a couple of cameras, basic telemetry, auto/teleop button, reconnect canfd bus button, allow basic info about motors (like power consumption?), battery power level, LATER- shows box of the map
 -->
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import RoverVelocityDisplay from '@/components/rover_velocity_display/RoverVelocityDisplay.vue';
+</script>
 <template>
   <div class="two-by-three-grid-page">
-    <h1>Not yet Implemented</h1>
+    <RoverVelocityDisplay />
     <h1>Not yet Implemented</h1>
     <h1>Not yet Implemented</h1>
     <h1>Not yet Implemented</h1>
